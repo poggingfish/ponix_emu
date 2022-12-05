19ab326b370ce5ece62e28bf7181603ccfeec744
Pogging <dylstructor@gmail.com>
2022-12-04 22:17:45 -0600

    Fixed start.sh

--- start.c
+++ start.c
@@ -3,5 +3,5 @@
 int main(){
     printf("ponix bootstrapper v1 (%s)\n", __VERSION__);
     printf("Making pvm\n");
-    system("clang pvm/src/pvm/pvm.c -o binaries/pvm");
+    system("mkdir -p binaries && clang pvm/src/pvm/pvm.c -o binaries/pvm");
 }
